36239f70625e52359872f63cb1dd92d6e923c799
Gonzalo Guzman <gonzalo.guzman@3it.cl>
2024-05-15 12:12:52 -0400

    init

---
+++ src/products/products.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, ParseIntPipe } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  //En un microservicio, ya no sería una petición HTTP, por lo que ya no recibiría los datos en un body, sino que en un Payload
+  //Por lo tanto, ya no se le pegará directamente a este controlador, como por ejemplo desde un Postman, sino que se deberá pegarle a un Gateway
+  //Luego, el Gateway le pegará a estas peticiones
+
+  @MessagePattern({ cmd: 'create_product' })
+  create(@Payload() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @MessagePattern({ cmd: 'find_all_products' })
+  findAll(@Payload() paginationDto: PaginationDto) {
+    return this.productsService.findAll(paginationDto);
+  }
+
+  @MessagePattern({ cmd: 'find_one_product' })
+  findOne(@Payload('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @MessagePattern({ cmd: 'update_product' })
+  update(
+    @Payload() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(updateProductDto.id, updateProductDto);
+  }
+
+  @MessagePattern({ cmd: 'delete_product' })
+  remove(@Payload('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
+  }
+}
